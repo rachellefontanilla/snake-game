6905c26b98fadbf2c2ebd798a258b25f4ec00d3b
Rachelle Fontanilla <rachfontanilla@gmail.com>
2021-02-09 23:50:01 -0500

    Add Snake Game

---
+++ main.py
@@ -0,0 +1,30 @@
+from food import Food
+from turtle import Screen
+from snake import Snake
+import time
+
+""" Screen Setup """
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
