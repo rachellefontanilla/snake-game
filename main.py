665ece69c725234fa7b870a1ff393cdcb5bfe9c5
Rachelle Fontanilla <rachfontanilla@gmail.com>
2021-02-10 23:54:45 -0500

    Update main.py with scoreboard class and other updated methods

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from food import Food
 from turtle import Screen
 from snake import Snake
+from scoreboard import Scoreboard
 import time
+
+BOUNDARY = 280
 
 """ Screen Setup """
 screen = Screen()
@@ -12,6 +15,7 @@ screen.title("My Snake Game")
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,10 +25,26 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
+    scoreboard.print_score()
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Detect collision with food
+    if snake.head.distance(food) < 16:
+        food.refresh()
+        snake.extend()
+        scoreboard.add_to_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > BOUNDARY or snake.head.xcor() < -BOUNDARY or snake.head.ycor() > BOUNDARY or snake.head.ycor() < -BOUNDARY:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
