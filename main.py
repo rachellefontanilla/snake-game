77d117fd93f4896064ca0a75bf359a8776a7abdb
Rachelle Fontanilla <rachfontanilla@gmail.com>
2021-02-16 22:42:31 -0500

    Add high score record

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    scoreboard.print_score()
     screen.update()
     time.sleep(0.1)
     snake.move()
@@ -43,13 +42,14 @@ while game_is_on:
         or snake.head.ycor() > BOUNDARY
         or snake.head.ycor() < -BOUNDARY
     ):
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,24 +1,42 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 22, "bold")
 
-class Scoreboard(Turtle):
 
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        # self.high_score = 0
+        with open("data.txt") as data:
+            high_score = data.read()
+            if high_score == "":
+                self.high_score = 0
+            else:
+                self.high_score = int(high_score)
         self.color("white")
-        self.penup
-        self.goto(0,270)
-            
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
     def add_to_score(self):
         self.score += 1
+        self.update_scoreboard()
 
-    def print_score(self):
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("data.txt", mode="w") as data:
+            data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
-        self.hideturtle()
-    
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        self.write(
+            f"Score = {self.score} High Score: {self.high_score}",
+            align=ALIGNMENT,
+            font=FONT,
+        )
