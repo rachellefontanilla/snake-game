a916b4f524ca3baa211ea0c8ccea1173bb945ae0
Rachelle Fontanilla <rachfontanilla@gmail.com>
2021-02-16 22:42:09 -0500

    Update formatting

--- food.py
+++ food.py
@@ -1,13 +1,13 @@
 from turtle import Turtle
 import random
 
-class Food(Turtle):
 
+class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5) 
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
         self.refresh()
@@ -16,4 +16,3 @@ class Food(Turtle):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
-
